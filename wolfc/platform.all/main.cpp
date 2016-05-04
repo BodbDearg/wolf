ef486b18ec9b4d4b66fae2b6e7850af5ae672c81
Darragh Coy <darraghcoy@runbox.com>
2016-05-04 00:07:43 -0700

    Compiling and linking against llvm framework now

--- wolfc/platform.all/main.cpp
+++ wolfc/platform.all/main.cpp
@@ -1,8 +1,18 @@
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
 #include <iostream>
 
-#include <llvm/IR/Module.h>
+// The context for the llvm compiler
+llvm::LLVMContext gLLVMContext;
+
+// The IR builder for the llvm compiler
+llvm::IRBuilder<> gIRBuilder(gLLVMContext);
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+    // The module, which holds all the code.
+    llvm::Module module("TestModule", gLLVMContext);
+    
+    // Dump the code to stdout
+    module.dump();
     return 0;
 }
