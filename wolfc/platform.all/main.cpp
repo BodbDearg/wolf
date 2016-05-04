cf1c7516090179d714ae0f59748261c01df8cb67
Darragh Coy <darraghcoy@runbox.com>
2016-05-04 01:31:51 -0700

    Generating the code for "Hello Wolf"

--- wolfc/platform.all/main.cpp
+++ wolfc/platform.all/main.cpp
@@ -3,7 +3,6 @@
 WC_THIRD_PARTY_INCLUDES_BEGIN
     #include <llvm/IR/Module.h>
     #include <llvm/IR/IRBuilder.h>
-    #include <iostream>
 WC_THIRD_PARTY_INCLUDES_END
 
 // The context for the llvm compiler
@@ -14,7 +13,35 @@ llvm::IRBuilder<> gIRBuilder(gLLVMContext);
 
 int main(int argc, const char * argv[]) {
     // The module, which holds all the code.
-    llvm::Module module("TestModule", gLLVMContext);
+    llvm::Module module("WolfTest", gLLVMContext);
+    
+    // Declare puts
+    llvm::FunctionType * putsFnType = llvm::FunctionType::get(llvm::Type::getInt32Ty(gLLVMContext),
+                                                              llvm::ArrayRef<llvm::Type*>
+                                                              {
+                                                                  llvm::Type::getInt8Ty(gLLVMContext)->getPointerTo()
+                                                              },
+                                                              false);
+    
+    llvm::Constant * putsFn = module.getOrInsertFunction("puts", putsFnType);
+    
+    // Create the function for main
+    llvm::FunctionType * mainFnType = llvm::FunctionType::get(llvm::Type::getVoidTy(gLLVMContext), {}, false);
+    llvm::Function * mainFn = llvm::Function::Create(mainFnType,
+                                                     llvm::Function::ExternalLinkage,
+                                                     "main",
+                                                     &module);
+    
+    // Create the block for main and set it as the insert point for ir builder
+    llvm::BasicBlock * mainBlock = llvm::BasicBlock::Create(gLLVMContext, "", mainFn);
+    gIRBuilder.SetInsertPoint(mainBlock);
+    
+    // Create string for hello world
+    llvm::Value * helloWorldStr = gIRBuilder.CreateGlobalStringPtr("Hello Wolf!");
+    gIRBuilder.CreateCall(putsFn, helloWorldStr);
+    
+    // Return void
+    gIRBuilder.CreateRetVoid();
     
     // Dump the code to stdout
     module.dump();
