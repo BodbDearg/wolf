081ac7c98777b3a79309fc37cc0c109a82a0ef0c
Darragh Coy <darraghcoy@runbox.com>
2016-11-16 00:08:22 -0800

    New data type that can store a value object but be lazy initialized

---
+++ wolfc/platform.all/WCLazyInit.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "WCAssert.hpp"
+#include "WCMacros.hpp"
+
+WC_BEGIN_NAMESPACE
+
+/**
+ * A wrapper for an object which is stored by value but which does not need to be initialized immediately.
+ * Can be used to store objects with the same efficiency as value semantics but without the restriction
+ * that the object must be constructed immediately.
+ */
+template <typename T>
+class LazyInit {
+public:
+    inline LazyInit() : mWasInitialized(false) {
+        // Don't initialize the object memory
+    }
+    
+    inline ~LazyInit() {
+        if (mWasInitialized) {
+            reinterpret_cast<T*>(mObjMem)->~T();
+        }
+    }
+    
+    inline T & operator * () const {
+        WC_ASSERT(mWasInitialized);
+        return *reinterpret_cast<T*>(const_cast<uint8_t*>(mObjMem));
+    }
+    
+    inline T * operator->() const {
+        WC_ASSERT(mWasInitialized);
+        return reinterpret_cast<T*>(const_cast<uint8_t*>(mObjMem));
+    }
+    
+    inline T & get() const {
+        WC_ASSERT(mWasInitialized);
+        return *reinterpret_cast<T*>(const_cast<uint8_t*>(mObjMem));
+    }
+    
+    inline bool wasInitialized() const {
+        return mWasInitialized;
+    }
+    
+    template <typename... Types>
+    inline void init(Types & ... params) {
+        WC_ASSERT(!mWasInitialized);
+        new (mObjMem) T(params...);
+        mWasInitialized = true;
+    }
+    
+private:
+    uint8_t     mObjMem[sizeof(T)];
+    bool        mWasInitialized;
+};
+
+WC_END_NAMESPACE
