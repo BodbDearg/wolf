84eb8d2718f58c04757084018f9ab4f57acfdc59
Darragh Coy <darraghcoy@runbox.com>
2016-11-20 23:58:48 -0800

    1 - Improvements to error logging for windows 2 - Found a problem case in .wolf file, saving for now

--- wolfc/platform.all/main.cpp
+++ wolfc/platform.all/main.cpp
@@ -15,56 +15,78 @@ WC_THIRD_PARTY_INCLUDES_END
 
 using namespace wolfc;
 
-static void msvcPauseForDebugger() {
-    #ifdef _MSC_VER
-        if (IsDebuggerPresent()) {
-            printf("Press enter to continue...");
-            getchar();
-        }
-    #endif
-}
-
-int main(int argc, const char * argv[]) {
+/* The actual implementation of main */
+static int mainImpl(int argc, const char * argv[]) {
     // Arg check
     if (argc != 2) {
-        std::printf("Usage: %s <Input Wolf File>\n", argv[0]);
-        msvcPauseForDebugger();
+        std::printf("Usage: %s <Input Wolf File>\n", argv[0]);        
         return -1;
     }
-    
+
     // Read the input file
     std::unique_ptr<char[]> inputSrc(wolfc::FileUtils::readTextFileAsCString(argv[1]));
-    
+
     if (!inputSrc) {
         std::printf("Failed to read input Wolf source file '%s'!\n", argv[1]);
         return -1;
     }
-    
+
     // The context for llvm compiler backend
     llvm::LLVMContext llvmContext;
-    
+
     // Run it through the lexer
     Lexer lexer;
     WC_GUARD(lexer.process(inputSrc.get()), -1);
-    
+
     // Linear allocator that we can use
     LinearAlloc linearAlloc(1024 * 1024,        /* 1 MB max alloc */
-                            1024 * 1024 * 16,   /* 16 MB block size */
-                            128,                /* Space in blocks array to reserve */
-                            1024 * 4);          /* Space in sys blocks array to reserve */
-    
+        1024 * 1024 * 16,   /* 16 MB block size */
+        128,                /* Space in blocks array to reserve */
+        1024 * 4);          /* Space in sys blocks array to reserve */
+
     {
         // Declare the module
         Module module(llvmContext);
-        
+
         // Parse and codegen the module
-        WC_GUARD(module.parseCode(lexer.getTokenList(), linearAlloc), -1);
-        WC_GUARD(module.generateCode(), -1);
-        
+        if (!module.parseCode(lexer.getTokenList(), linearAlloc)) {
+            std::printf("Parsing failed for source file '%s'!\n", argv[1]);
+            return -1;
+        }
+
+        if (!module.generateCode()) {
+            std::printf("Compile failed for source file '%s'!\n", argv[1]);
+            return -1;
+        }
+
         // Dump the code to stdout!
         module.dumpIRCodeToStdout();
     }
-    
-    msvcPauseForDebugger();
+
     return 0;
+}
+
+/* In windows builds, pauses for the debugger. Does nothing in other builds. */
+static void msvcPauseForDebugger() {
+#ifdef _MSC_VER
+    if (IsDebuggerPresent()) {
+        printf("Press enter to continue...");
+        getchar();
+    }
+#endif
+}
+
+/* Program entry point */
+int main(int argc, const char * argv[]) {
+    int statusCode = -1;
+
+    try {
+        statusCode = mainImpl(argc, argv);
+    }
+    catch (...) {
+        printf("An uncaught exception occurred! Unsure what the problem was.");
+    }
+
+    msvcPauseForDebugger();
+    return statusCode;
 }
