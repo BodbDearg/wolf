b386051608cc27441c2bd1a3c6753e653f81ab9e
Darragh Coy <darraghcoy@runbox.com>
2016-11-20 17:29:25 -0800

    Build is very close to working now for wolfc test - just need to figure out the linker errors

--- wolfc/platform.win/wolfc-test.bat
+++ wolfc/platform.win/wolfc-test.bat
@@ -1,29 +1,50 @@
-@IF NOT EXIST "wolfc_test" (
-	@mkdir wolfc_test
+@REM ** Setting up build paths ** 
+@SET VS_DIR=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+@SET MSVC_DIR=%VS_DIR%\VC
+@SET WIN_SDK_DIR=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.14393.0\um\x64
+@SET INCLUDE=%MSVC_DIR%\INCLUDE;%MSVC_DIR%\VC\ATLMFC\INCLUDE;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\ucrt;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\include\um;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\shared;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\um;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\winrt;
+@SET LIB=%MSVC_DIR%\LIB;%MSVC_DIR%\ATLMFC\LIB;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\lib\um\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\um\x64;
+@SET LIBPATH=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\LIB;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\ATLMFC\LIB;C:\Program Files (x86)\Windows Kits\10\UnionMetadata;C:\Program Files (x86)\Windows Kits\10\References;C:\Program Files (x86)\Microsoft SDKs\Windows Kits\10\ExtensionSDKs\Microsoft.VCLibs\14.0\References\CommonConfiguration\neutral;
+
+@REM ** Make the output folder if it does not exist ** 
+@IF NOT EXIST "wolfc-test" (
+	@MKDIR wolfc-test
 )
 
-@"x64\Debug\wolfc.exe" "..\test_data\wolfc-test.wolf" 2>"wolfc_test\wolfc-test.ll"
+@REM ** Run the compiler ** 
+@"x64\Debug\wolfc.exe" "..\test_data\wolfc-test.wolf" 2>"wolfc-test\wolfc-test.ll"
 
 @IF %ERRORLEVEL% NEQ 0 (
-	@more "wolfc_test\wolf-test.ll"
-	@pause
-	@exit %ERRORLEVEL%
+	@MORE "wolfc_test\wolf-test.ll"
+	@PAUSE
+	@EXIT /B %ERRORLEVEL%
 )
 
-@llc -march=x86-64 -x86-asm-syntax=intel -o "wolfc_test\wolfc-test.s" "wolfc_test\wolfc-test.ll"
+@REM ** Convert IR code to assembly code ** 
+@llc -march=x86-64 -x86-asm-syntax=intel -o "wolfc-test\wolfc-test.s" "wolfc-test\wolfc-test.ll"
+
+@IF %ERRORLEVEL% NEQ 0 (
+	@PAUSE
+	@EXIT /B %ERRORLEVEL%
+)
+
+@REM ** Assemble assembly code to an object file **
+@llvm-mc -arch=x86-64 -filetype=obj -assemble "wolfc-test\wolfc-test.s" -o="wolfc-test\wolfc-test.o"
 
 @IF %ERRORLEVEL% NEQ 0 (
 	@pause
-	@exit %ERRORLEVEL%
+	@EXIT /B %ERRORLEVEL%
 )
 
-@clang "wolfc_test\wolfc-test.s" -o "wolfc_test\wolfc-test"
+@REM ** Link the object file **
+@lld-link "wolfc-test\wolfc-test.o"
 
 @IF %ERRORLEVEL% NEQ 0 (
 	@pause
-	@exit %ERRORLEVEL%
+	@EXIT /B %ERRORLEVEL%
 )
 
+@REM ** Run the actual program **
 "wolfc_test\wolfc-test"
-@echo Program exited with status %ERRORLEVEL%.
-pause
+@ECHO Program exited with status %ERRORLEVEL%.
+@PAUSE
