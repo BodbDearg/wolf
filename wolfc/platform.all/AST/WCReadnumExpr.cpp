8ead625841ab9e01766bae1468ea5cb39b3fc98a
Darragh Coy <darraghcoy@runbox.com>
2016-11-27 19:51:32 -0800

    Some small tweaks

--- wolfc/platform.all/AST/WCReadnumExpr.cpp
+++ wolfc/platform.all/AST/WCReadnumExpr.cpp
@@ -86,18 +86,18 @@ llvm::Value * ReadnumExpr::codegenExprEval(CodegenCtx & cgCtx) {
     }
     
     // Create a format string for scanf
-    llvm::Value * fmtStr = cgCtx.irBuilder.CreateGlobalStringPtr("%zd", "readnum_expr_fmt_str");
+    llvm::Value * fmtStr = cgCtx.irBuilder.CreateGlobalStringPtr("%zd", "ReadnumExpr:fmt_str");
     
     // Create a stack var to hold the output:
     llvm::Value * outputVar = cgCtx.irBuilder.CreateAlloca(cgCtx.irBuilder.getInt64Ty(),
                                                            nullptr,
-                                                           "readnum_expr_temp_stack_var");
+                                                           "ReadnumExpr:tmp_stack_var");
     
     // Create the call to scanf!
-    cgCtx.irBuilder.CreateCall(scanfFn, { fmtStr, outputVar }, "readnum_expr_scanf_call");
+    cgCtx.irBuilder.CreateCall(scanfFn, { fmtStr, outputVar }, "ReadnumExpr:scanf_call");
     
     // And return the stack var
-    return cgCtx.irBuilder.CreateLoad(outputVar, "readnum_expr_load_temp_stack_var");
+    return cgCtx.irBuilder.CreateLoad(outputVar, "ReadnumExpr:load_tmp_stack_var");
 }
 
 llvm::Constant * ReadnumExpr::codegenExprConstEval(CodegenCtx & cgCtx) {
