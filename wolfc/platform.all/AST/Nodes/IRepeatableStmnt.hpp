11bd58adf4b28d70f9cece755d4383605ec19646
Darragh Coy <darraghcoy@runbox.com>
2017-02-02 00:38:47 -0800

    Remove todo

--- wolfc/platform.all/AST/Nodes/IRepeatableStmnt.hpp
+++ wolfc/platform.all/AST/Nodes/IRepeatableStmnt.hpp
@@ -9,21 +9,6 @@ WC_AST_BEGIN_NAMESPACE
 class IRepeatableStmnt : public IStmnt {
 public:
     virtual ~IRepeatableStmnt() = default;
-    
-#warning FIXME - Codegen
-#if 0
-    /**
-     * Returns the block that must be jumped to in order for the loop to skip to the next iteration.
-     * This is the block that the 'next' statement will jump to.
-     */
-    virtual llvm::BasicBlock * getNextStmntTargetBlock() = 0;
-    
-    /**
-     * Returns the block that must be jumped to in order for the loop to be broken out of.
-     * This is the block that the 'break' statement will jump to.
-     */
-    virtual llvm::BasicBlock * getBreakStmntTargetBlock() = 0;
-#endif
 };
 
 WC_AST_END_NAMESPACE
