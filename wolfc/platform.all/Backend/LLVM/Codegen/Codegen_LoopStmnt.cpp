a9d9b70b17a3b8037951f45547100090aa80c738
Darragh Coy <darraghcoy@runbox.com>
2017-02-09 23:49:50 -0800

    Fix another bug with deferred codegen in loop statements

--- wolfc/platform.all/Backend/LLVM/Codegen/Codegen_LoopStmnt.cpp
+++ wolfc/platform.all/Backend/LLVM/Codegen/Codegen_LoopStmnt.cpp
@@ -144,6 +144,9 @@ void Codegen::visit(const AST::LoopStmntWithCond & astNode) {
     
     // Insert code after the end block from here on in
     mCtx.mIRBuilder.SetInsertPoint(endBB);
+    
+    // Do any deferred codegen which needs to be done when the loop is finished
+    mCtx.handleDeferredCodegenCallbacks(repeatableStmnt.mDeferredCodegenCallbacks);
 }
 
 WC_LLVM_BACKEND_END_NAMESPACE
