edb091fc9c7fd22723f7037be9823efa7b1c5b1d
Darragh Coy <darraghcoy@runbox.com>
2017-03-01 00:59:16 -0800

    Make the runtime boolean literal code generator use the constant code generator for this literal - same thing.

--- wolfc/platform.all/Backend/LLVM/Codegen/Codegen_BoolLit.cpp
+++ wolfc/platform.all/Backend/LLVM/Codegen/Codegen_BoolLit.cpp
@@ -8,10 +8,6 @@
 
 #include "../CodegenCtx.hpp"
 #include "AST/Nodes/BoolLit.hpp"
-#include "Assert.hpp"
-#include "DataType/Types/BoolDataType.hpp"
-#include "DataType/Types/PrimitiveDataTypes.hpp"
-#include "Lexer/Token.hpp"
 
 WC_BEGIN_NAMESPACE
 WC_LLVM_BACKEND_BEGIN_NAMESPACE
@@ -19,22 +15,13 @@ WC_LLVM_BACKEND_BEGIN_NAMESPACE
 void Codegen::visit(const AST::BoolLit & astNode) {
     WC_CODEGEN_RECORD_VISITED_NODE();
     
-    #warning TODO: Use constant code generator
-    // Codegen the data type for the int
-    {
-        const BoolDataType & dataType = PrimitiveDataTypes::getBoolDataType();
-        dataType.accept(mCodegenDataType);
-    }
+    // This just uses the constant code generator, it's the same thing:
+    astNode.accept(mConstCodegen);
     
-    CompiledDataType compiledType = mCtx.popCompiledDataType();
-    
-    // Create the value and save to the stack
-    WC_ASSERT(compiledType.isValid());
-    llvm::Value * llvmValue = astNode.mToken.type == TokenType::kTrue ?
-        mCtx.mIRBuilder.getTrue() :
-        mCtx.mIRBuilder.getFalse();
-    
-    mCtx.pushValue(Value(llvmValue, compiledType, false, &astNode));
+    // Repackage the result as a non constant however:
+    Constant constant = mCtx.popConstant();
+    WC_GUARD(constant.isValid());
+    mCtx.pushValue(Value(constant.mLLVMConst, constant.mCompiledType, false, &astNode));
 }
 
 WC_LLVM_BACKEND_END_NAMESPACE
