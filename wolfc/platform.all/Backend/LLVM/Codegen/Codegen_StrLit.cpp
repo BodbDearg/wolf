9a87e2e2bea7c7d18503bb7dcb7e1e1b678ffc08
Darragh Coy <darraghcoy@runbox.com>
2017-03-09 22:55:26 -0800

    Use the constant code generator for runtime string literal - does the same thing

--- wolfc/platform.all/Backend/LLVM/Codegen/Codegen_StrLit.cpp
+++ wolfc/platform.all/Backend/LLVM/Codegen/Codegen_StrLit.cpp
@@ -7,36 +7,21 @@
 #include "Codegen.hpp"
 
 #include "../CodegenCtx.hpp"
+#include "../ConstCodegen/ConstCodegen.hpp"
 #include "AST/Nodes/StrLit.hpp"
-#include "Assert.hpp"
-#include "DataType/DataType.hpp"
-#include "DataType/Types/PrimitiveDataTypes.hpp"
-#include "Lexer/Token.hpp"
 
 WC_BEGIN_NAMESPACE
 WC_LLVM_BACKEND_BEGIN_NAMESPACE
 
 void Codegen::visit(const AST::StrLit & astNode) {
-    WC_CODEGEN_RECORD_VISITED_NODE();
+    // Use the constant code generator to do this since it's just the same thing, but convert the
+    // result over to a non constant value:
+    astNode.accept(mConstCodegen);
+    Constant constant = mCtx.popConstant();
     
-    #warning TODO: Use constant code generator
-    // Codegen the data type for the string
-    {
-        const DataType & dataType = PrimitiveDataTypes::getUsingTypeId(DataTypeId::kStr);
-        dataType.accept(mCodegenDataType);
+    if (constant.isValid()) {
+        mCtx.pushValue(Value(constant.mLLVMConst, constant.mCompiledType, false, &astNode));
     }
-    
-    CompiledDataType compiledType = mCtx.popCompiledDataType();
-    
-    // Codegen the string literal and save
-    // TODO: need some sort of LUT for the module so we only have unique string instances
-    llvm::Value * strVal = mCtx.mIRBuilder.CreateGlobalStringPtr(
-        astNode.mToken.data.strVal.ptr,
-        "StrLit"
-    );
-    
-    WC_ASSERT(strVal);
-    mCtx.pushValue(Value(strVal, compiledType, false, &astNode));
 }
 
 WC_LLVM_BACKEND_END_NAMESPACE
