944825a4c7314a4852f16c2dfb9ca94e162b993b
Darragh Coy <darraghcoy@runbox.com>
2017-03-10 00:06:54 -0800

    Bug fix issues with TernaryExpr and lazy evaluation

--- wolfc/platform.all/Backend/LLVM/Codegen/Codegen_TernaryExpr.cpp
+++ wolfc/platform.all/Backend/LLVM/Codegen/Codegen_TernaryExpr.cpp
@@ -58,42 +58,55 @@ void Codegen::visit(const AST::TernaryExprWithCond & astNode) {
     llvm::Function * parentFn = mCtx.mIRBuilder.GetInsertBlock()->getParent();
     WC_ASSERT(parentFn);
     
-    // Create a block for the true, false and follow on expressions:
+    // Create a basic block for the true and false follow on expressions:
     std::string trueBBLbl = StringUtils::appendLineInfo("TernaryExprWithCond:true", astNode.mTrueExpr.getStartToken());
     llvm::BasicBlock * trueBB = llvm::BasicBlock::Create(mCtx.mLLVMCtx, trueBBLbl, parentFn);
+    WC_ASSERT(trueBB);
     
     std::string falseBBLbl = StringUtils::appendLineInfo("TernaryExprWithCond:false", astNode.mFalseExpr.getStartToken());
     llvm::BasicBlock * falseBB = llvm::BasicBlock::Create(mCtx.mLLVMCtx, falseBBLbl, parentFn);
-    
-    std::string endBBLbl = StringUtils::appendLineInfo("TernaryExprWithCond:end", astNode.getPastEndToken());
-    llvm::BasicBlock * endBB = llvm::BasicBlock::Create(mCtx.mLLVMCtx, endBBLbl, parentFn);
+    WC_ASSERT(falseBB);
     
     // Need to save the current insert block so we can return to it later
     mCtx.pushInsertBlock();
     
-    // Evaluate the true expression value and the false expression value
+    // Evaluate the true expression value and the false expression value.
+    // Note: save the end basic blocks for both expressions since they might differ (may have lazy eval for instance)
     mCtx.mIRBuilder.SetInsertPoint(trueBB);
     astNode.mTrueExpr.accept(*this);
     Value trueVal = mCtx.popValue();
-    WC_ASSERTED_OP(mCtx.mIRBuilder.CreateBr(endBB));
+    llvm::BasicBlock * trueEndBB = mCtx.mIRBuilder.GetInsertBlock();
+    WC_ASSERT(trueEndBB);
     
     mCtx.mIRBuilder.SetInsertPoint(falseBB);
     astNode.mFalseExpr.accept(*this);
     Value falseVal = mCtx.popValue();
-    WC_ASSERTED_OP(mCtx.mIRBuilder.CreateBr(endBB));
+    llvm::BasicBlock * falseEndBB = mCtx.mIRBuilder.GetInsertBlock();
+    WC_ASSERT(falseEndBB);
     
     // Determine if we should do implicit casts so the type of both expressions
     // matches, and if we should cast in which direction:
     ImplicitCasts::BinaryOpCastDir castDir = ImplicitCasts::determineCastDirForBinaryOp(mCtx, trueVal, falseVal);
     
     if (castDir == ImplicitCasts::BinaryOpCastDir::kCastLeftToRight) {
-        mCtx.mIRBuilder.SetInsertPoint(trueBB);
+        mCtx.mIRBuilder.SetInsertPoint(trueEndBB);
         ImplicitCasts::castSingleValueIfRequired(*this, trueVal, falseVal.mCompiledType);
     }
     else if (castDir == ImplicitCasts::BinaryOpCastDir::kCastRightToLeft) {
-        mCtx.mIRBuilder.SetInsertPoint(falseBB);
+        mCtx.mIRBuilder.SetInsertPoint(falseEndBB);
         ImplicitCasts::castSingleValueIfRequired(*this, falseVal, trueVal.mCompiledType);
     }
+    
+    // Create the end basic block:
+    std::string endBBLbl = StringUtils::appendLineInfo("TernaryExprWithCond:end", astNode.getPastEndToken());
+    llvm::BasicBlock * endBB = llvm::BasicBlock::Create(mCtx.mLLVMCtx, endBBLbl, parentFn);
+    WC_ASSERT(endBB);
+    
+    // Create the branches at the end of the true and false basic blocks to the end basic block
+    mCtx.mIRBuilder.SetInsertPoint(trueEndBB);
+    WC_ASSERTED_OP(mCtx.mIRBuilder.CreateBr(endBB));
+    mCtx.mIRBuilder.SetInsertPoint(falseEndBB);
+    WC_ASSERTED_OP(mCtx.mIRBuilder.CreateBr(endBB));
     
     // Return to the previously saved insert block
     mCtx.popInsertBlock();
@@ -135,8 +148,8 @@ void Codegen::visit(const AST::TernaryExprWithCond & astNode) {
     // Create a PHI node to merge the values:
     llvm::PHINode * phiNode = mCtx.mIRBuilder.CreatePHI(trueVal.mCompiledType.getLLVMType(), 2, "TernaryExprWithCond:PHI");
     WC_ASSERT(phiNode);
-    phiNode->addIncoming(trueVal.mLLVMVal, trueBB);
-    phiNode->addIncoming(falseVal.mLLVMVal, falseBB);
+    phiNode->addIncoming(trueVal.mLLVMVal, trueEndBB);
+    phiNode->addIncoming(falseVal.mLLVMVal, falseEndBB);
     
     // The PHI node is the result of the operation:
     mCtx.pushValue(Value(phiNode, trueVal.mCompiledType, false, &astNode));
